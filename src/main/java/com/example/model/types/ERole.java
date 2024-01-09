5168fd05e3804f3c9a604200933ef7694b616381
Elcheno <r.c.a2312@gmail.com>
2024-01-09 23:46:45 +0100

    first commit

---
+++ src/main/java/com/example/model/types/ERole.java
@@ -0,0 +1,5 @@
+package com.example.model.types;
+
+public enum ERole {
+    ADMIN, USER, INVITED
+}
---
+++ src/main/java/com/example/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+
+import com.example.model.entity.RoleEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<RoleEntity, Long> {}
